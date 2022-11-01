db937855dc15f8f584c3c9319a087a29c9bcb8aa
Michael Su <mailmsuu@gmail.com>
2022-11-01 10:51:21 +0000

    Game now initialised by given word, getWordToGuess now create hidden word based on the given word

--- app/src/main/java/game/Game.java
+++ app/src/main/java/game/Game.java
@@ -1,7 +1,21 @@
 package game;
 
 public class Game {
-    public static String getWordToGuess() {
-        return "B_____";
+    private String word;
+    public Game(String word) {
+        this.word = word;
+    }
+
+    public String getWordToGuess() {
+        StringBuilder hiddenWord = new StringBuilder();
+        for (Integer i = 0; i < this.word.length(); i++ ) {
+            Character currentLetter = this.word.charAt(i);
+            if (i == 0) {
+                hiddenWord.append(currentLetter);
+            } else {
+                hiddenWord.append("_");
+            }
+        }
+        return hiddenWord.toString();
     }
 }
--- app/src/test/java/game/GameTest.java
+++ app/src/test/java/game/GameTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class GameTest {
     @Test public void testGetsWordToGuess() {
-        Game game = new Game();
-        assertEquals(game.getWordToGuess(), "B_____");
+        Game game = new Game("MAKERS");
+        assertEquals("Game initialises with given word", "M_____", game.getWordToGuess());
     }
 }
