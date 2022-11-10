bbc13c1b93879cdd3956248f82713f17b433c18b
Michael Su <mailmsuu@gmail.com>
2022-11-10 16:00:10 +0000

    can see results of test in terminal, but custom function to compare not working

--- app/src/test/java/game/AppTest.java
+++ app/src/test/java/game/AppTest.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 
 public class AppTest {
     private static final WordChoser mockChoser = mock(WordChoser.class);
-    private static final Masker mockMasker = mock(Masker.class);
 
     @Before public void setupTests() {
         when(mockChoser.getRandomWordFromDictionary()).thenReturn("MAKERS");
@@ -27,6 +26,26 @@ public class AppTest {
         assertEquals("Maker: M_____ \n", appOutput[2]);
 
         assertEquals("\nMaker: Enter one letter to guess: (10 attempts remaining): \n", appOutput[3]);
+        assertEquals("Right!): \n", appOutput[4]);
+        assertEquals("Maker: MA____): \n", appOutput[5]);
+
+        assertEquals("\nMaker: Enter one letter to guess: (10 attempts remaining): \n", appOutput[6]);
+        assertEquals("Right!): \n", appOutput[7]);
+        assertEquals("Maker: MAK___): \n", appOutput[8]);
+
+        assertEquals("\nMaker: Enter one letter to guess: (10 attempts remaining): \n", appOutput[9]);
+        assertEquals("Right!): \n", appOutput[10]);
+        assertEquals("Maker: MAKE__): \n", appOutput[11]);
+
+        assertEquals("\nMaker: Enter one letter to guess: (10 attempts remaining): \n", appOutput[12]);
+        assertEquals("Right!): \n", appOutput[13]);
+        assertEquals("Maker: MAKER_): \n", appOutput[14]);
+
+        assertEquals("\nMaker: Enter one letter to guess: (10 attempts remaining): \n", appOutput[15]);
+        assertEquals("Right!): \n", appOutput[16]);
+        assertEquals("Maker: MAKERS): \n", appOutput[17]);
+
+        assertEquals("Congratulations Maker! The word was MAKERS.", appOutput[18]);
     }
 
     private String[] runApp(String userInput) throws IOException {
@@ -42,7 +61,7 @@ public class AppTest {
             }
         };
 
-        App app = new App(input, new PrintStream(output), mockChoser, mockMasker);
+        App app = new App(input, new PrintStream(output), mockChoser, new Masker());
         app.multiPlayer(0, 1);
 
         // modify captured to something that is testable
